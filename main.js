e58c67da3162b4d02e1910432ebe371a025548c3
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2023-08-27 11:06:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+//validate email//
+
+// const email = document.getElementById("email").value;
+
+
+// function validarEmail (emailCapturadoDoFormulario) {
+//     let validarRegExNoEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//         if (emailCapturadoDoFormulario.match(validarRegExNoEmail)) {
+//         console.log("RegEx válido!");
+//         document.getElementById("email").style.backgroundColor = "white";
+//         document.getElementById("email").style.color = "black";
+//         document.getElementById("aviso-erro").style.display = "none";
+//         return true;
+//     } else {
+//         console.log("RegEx Inválido");
+//         document.getElementById("email").style.backgroundColor = "red";
+//         document.getElementById("email").style.color = "white";
+//         document.getElementById("aviso-erro").style.display = "block";
+//         return false;
+//     }
+// }
+///////////////////////////////////////////////////////////////////////////
+
 //password//
 const password = document.getElementById("password");
 const confirm_password = document.getElementById("confirm_password");
@@ -12,3 +35,4 @@ function validatePassword(){
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+
